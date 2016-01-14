6eb8fc3d3cef243bc726a2bb4265674cd8051e1c
Adam B.-Bolduc <adam.b.bolduc@gmail.com>
2016-01-13 22:17:30 -0500

    refactor des tests afin definir un seul deftest pour une fonction donnee

--- test/clj/casino2016/game_test.clj
+++ test/clj/casino2016/game_test.clj
@@ -7,32 +7,34 @@
 (def ^:const new-player (player a-name))
 (def ^:const n 6)
 (def ^:const a-player (give-n-tickets n new-player))
+(def a-new-game (new-game))
 
 
-(deftest new-player-has-zero-tickets
+(deftest new-player-initialization
+  (testing "A new player has zero tickets"
     (is (= zero-tickets (:tickets new-player))))
-
-(deftest new-player-has-a-name
+  (testing "A new player has a name"
     (is (= a-name (new-player :name))))
+  (testing "A new player has no choice"
+    (is (= :nochoice (new-player :choice))))
+  (testing "A new player has not lost... yet"
+    (is (not (:lost new-player)))))
 
-(deftest new-player-has-no-choice
-  (is (= :nochoice (new-player :choice))))
-
-(deftest new-player-has-not-lost-yet
-  (is (not (:lost new-player))))
-
-(deftest given-a-player-when-give-n-tickets-player-has-n-more-tickets
+(deftest give-n-tickets-test
   (let [m 4
         prev-number-of-tickets (:tickets a-player)
         new-number-of-tickets (:tickets (give-n-tickets m a-player))]
-    (is (= (+ prev-number-of-tickets m) new-number-of-tickets))))
+    (testing "Given a player when give n tickets player has n more tickets"
+      (is (= (+ prev-number-of-tickets m) new-number-of-tickets)))))
 
 
-(deftest new-game-has-no-players
-  (is (empty? (:players (new-game)))))
+(deftest new-game-initialization
+  (testing "A new game has no players"
+    (is (empty? (:players a-new-game)))))
 
-(deftest when-add-player-on-game-then-player-is-present
-  (let [game (add-player a-player (new-game))]
-    (is (not-empty
-     (filter #(= (:name %) (:name a-player))
-            (:players game))))))
+(deftest add-player-test
+  (let [game (add-player a-player a-new-game)]
+    (testing "Given a new game when adding a player then a player is present in the game"
+      (is (not-empty
+           (filter #(= (:name %) (:name a-player))
+                   (:players game)))))))
