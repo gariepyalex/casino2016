72c8ce109579a0b37e5960837a843845a451bba3
Alexandre Gariepy <gariepy.alex@gmail.com>
2016-01-22 17:53:22 -0500

    readded figwheel in dev cljs profile

--- project.clj
+++ project.clj
@@ -26,6 +26,7 @@
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
 
   :cljsbuild {:builds [{:id "dev"
+                        :figwheel {:on-jsload "casino2016.core/on-js-reload"}
                         :source-paths ["src/cljs"]
                         :compiler {:main casino2016.core
                                    :asset-path "js/compiled/out"
